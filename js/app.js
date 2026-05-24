e711fefe7acabf56c92ebdcf354cba609779a839
Maisc-fm <maisarah.cedric@gmail.com>
2026-05-24 19:04:23 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
+function loadScreen(screen) {
+  fetch(`screens/${screen}.html`)
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("content").innerHTML = data;
+    });
+}
 
+/* Load default screen */
+window.onload = () => {
+  loadScreen("home");
+};
+
+/* Climate logic */
+let temp = 24;
+
+function changeTemp(val) {
+  temp += val;
+  document.getElementById("temp").innerText = temp;
+}
